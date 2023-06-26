9c1f3496efeff8e33473f5c67e639291f6b8abd9
Asmita <asmitade.8940@gmail.com>
2023-06-26 14:33:12 -0400

    Initial Commit

---
+++ Zen Zone/Zen_ZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zen_ZoneApp.swift
+//  Zen Zone
+//
+//  Created by Asmita De on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Zen_ZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
